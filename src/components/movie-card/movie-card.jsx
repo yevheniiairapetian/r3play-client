98b3ebb60c7a8f550f387a870b43245230d48f9e
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-08-28 13:41:14 +0200

    feat: fix errors

--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -1,3 +1,5 @@
+import PropTypes from "prop-types";
+
 export const MovieCard = ({ movie, onMovieClick }) => {
     return (
       <div
@@ -5,8 +7,29 @@ export const MovieCard = ({ movie, onMovieClick }) => {
           onMovieClick(movie);
         }}
       >
-        {movie.title}
+        {movie.Title}
       </div>
     );
   };
   
+  MovieCard.propTypes = {
+    movie: PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      Title: PropTypes.string.isRequired,
+      Description: PropTypes.string.isRequired,
+      Genre: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Description: PropTypes.string.isRequired}),
+      Director: PropTypes.shape({
+        Name: PropTypes.string.isRequired,
+        Bio: PropTypes.string.isRequired,
+        Birth: PropTypes.string 
+      }),
+      ImagePath: PropTypes.string,
+      Featured: PropTypes.bool,
+      Actors: PropTypes.array,
+      Rating: PropTypes.string,
+      ReleaseDate: PropTypes.string
+}).isRequired,
+    onMovieClick: PropTypes.func.isRequired
+  };
