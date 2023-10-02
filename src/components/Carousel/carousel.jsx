f317ba05f2417540375ad971e168aa0043f4e756
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2023-10-02 20:19:46 +0200

    feat: update carousel styling
    Change carousel button color

--- src/components/Carousel/carousel.jsx
+++ src/components/Carousel/carousel.jsx
@@ -11,7 +11,7 @@ export const UncontrolledExample = () => {
           <Carousel.Caption>
             <h3 className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-header">Gladiator 2 releases November 22, 2024</h3>
             <Link to={`/movies/64ee548c05e13ad776e073a3`}>
-            <Button className='info-button p4-5 pr-4' variant='outline-light'>Check out more</Button>
+            <Button className='bg-success text-light p4-5 pr-4' variant='outline-dark'>Check out more</Button>
           </Link>
             {/* <p className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-paragraph">Check out more</p> */}
           </Carousel.Caption>
@@ -21,7 +21,7 @@ export const UncontrolledExample = () => {
           <Carousel.Caption>
             <h3 className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-header">Matrix Resurrections is out now</h3>
             <Link to={`/movies/64ee1dcd3cd020929155cccf`}>
-            <Button className='info-button p4-5 pr-4' variant='outline-light'>Check out more</Button>
+            <Button className='bg-success text-light p4-5 pr-4' variant='outline-dark'>Check out more</Button>
           </Link>
             {/* <p className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-paragraph">Head over to the movie to read more and watch the official trailer.</p> */}
           </Carousel.Caption>
@@ -31,7 +31,7 @@ export const UncontrolledExample = () => {
           <Carousel.Caption>
             <h3 className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-header">Loki: Season 2 releases October 5, 2023</h3>
             <Link to={`/tvseries/650e060abda39eddcd984e75`}>
-            <Button className='info-button p4-5 pr-4' variant='outline-light'>Check out more</Button>
+            <Button className='bg-success text-light p4-5 pr-4' variant='outline-dark'>Check out more</Button>
           </Link>
             {/* <p className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-paragraph">
               Check out more.
@@ -43,7 +43,7 @@ export const UncontrolledExample = () => {
           <Carousel.Caption>
             <h3 className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-header">Friends Reunion out 2021</h3>
             <Link to={`/tvseries/65108f77126939ab39597d7c`}>
-            <Button className='info-button p4-5 pr-4' variant='outline-light'>Check out more</Button>
+            <Button className='bg-success text-light p4-5 pr-4' variant='outline-dark'>Check out more</Button>
           </Link>
             {/* <p className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-paragraph">
               Check out more.
@@ -55,7 +55,7 @@ export const UncontrolledExample = () => {
           <Carousel.Caption>
             <h3 className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-header">Recall the Fast and Furious: Tokyo Drift - the 3rd movie in the series</h3>
             <Link to={`/movies/64bfcd6b9f48ba9eb027fc65`}>
-            <Button className='info-button p4-5 pr-4' variant='outline-light'>Check out more</Button>
+            <Button className='bg-success text-light p4-5 pr-4' variant='outline-dark'>Check out more</Button>
           </Link>
             {/* <p className="bg-dark pt-3 pb-3 bg-carousel carousel-caption-paragraph">
               Check out more.
