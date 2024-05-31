1eef1937f1766bd2e19c776ca07db09daf458d78
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-05-31 21:53:57 +0200

    test: test add user image

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -165,15 +165,16 @@ export const ProfileView = ({ user, token, setUser, movies, tvseries, animes }) 
 								required
 							/>
 						</Form.Group>
-						{/* <Form.Group controlId="formImage" className='form-group pb-4'>
+						<Form.Group controlId="formImage" className='form-group pb-4'>
 							<Form.Label className="profie-image form-label" >Profile Image:</Form.Label>
 							<Form.Control
 								type="file"
 								accept="image/*"
 								value={image}
 								onChange={(e) => setImage(e.target.value)}
+							// required
 							/>
-						</Form.Group> */}
+						</Form.Group>
 
 						<Col>
 							<Button className="form-submit-button w-100 mb-1" type="submit" onClick={handleSubmit}>Update</Button>
