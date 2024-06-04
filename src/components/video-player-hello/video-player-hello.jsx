cefa2091ac361b942b1a959772374fee08a3fae1
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-06-04 18:39:46 +0200

    style: remove picture in picture mode from the welcome video player

--- src/components/video-player-hello/video-player-hello.jsx
+++ src/components/video-player-hello/video-player-hello.jsx
@@ -3,7 +3,7 @@ import ReactPlayer from 'react-player'
 
 export const VideoPlayerHello = () => {
     return(
-        <ReactPlayer className="welcome-player"  playing="true" loop="true"  volume="0"  pip="true" stopOnUnmount="false" url='https://videos.pexels.com/video-files/4210523/4210523-hd_1280_720_24fps.mp4' />
+        <ReactPlayer className="welcome-player"  playing="true" loop="true"  volume="0"  stopOnUnmount="false" url='https://videos.pexels.com/video-files/4210523/4210523-hd_1280_720_24fps.mp4' />
     )
 }
 
