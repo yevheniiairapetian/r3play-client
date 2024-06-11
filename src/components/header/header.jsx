ce056776664768e801db7196b73ff2fe43790df2
Yevhenii Airapetian <sonkozhenia11@gmail.com>
2024-06-11 22:03:01 +0200

    feat: update main view header text

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,7 +11,7 @@ export const Header = () => {
         <div className='mask' style={{ backgroundColor: 'rgba(0, 0, 0, 0.6)' }}>
           <div className='d-flex justify-content-center align-items-center pb-4'>
             <div className='text-white'>
-              <h1 className='pt-4 mb-3'>Movies, Anime and TV Series</h1>
+              <h1 className='pt-4 mb-3'>Movies, Anime, TV Series and more</h1>
               <h4 className='pt-3 mb-3'>Explore the library</h4>
               <Link to={`/movies`}>
               <button className="check-more-header-btn pt-2 pb-2 pl-4 pr-4 w-50" variant="outline-light">
